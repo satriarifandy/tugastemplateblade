d10d80e0da35bd9aa5648df48a76678316a55c2a
arifandy <satriarifandy@gmail.com>
2020-07-01 20:01:36 +0700

    ngubah script js

--- resources/views/items/datatables.blade.php
+++ resources/views/items/datatables.blade.php
@@ -443,8 +443,8 @@
 
 @push('scripts')
 
-<script src=" {{ asset(' adminlte/plugins/datatables/jquery.dataTables.js ') }}"></script>
-<script src=" {{ asset( 'adminlte/plugins/datatables-bs4/js/dataTables.bootstrap4.js' ) }}"></script>
+<script src="{{ asset('/adminlte/plugins/datatables/jquery.dataTables.js')}}"></script>
+<script src="{{ asset('/adminlte/plugins/datatables-bs4/js/dataTables.bootstrap4.js')}}"></script>
 <script>
   $(function () {
     $("#example1").DataTable();
